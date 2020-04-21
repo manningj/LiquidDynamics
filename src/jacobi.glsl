540dcfc8febbf3a250ab5f5b01190c81a1ed2859
John Manning <john.manning144@gmail.com>
2020-04-20 23:38:45 -0500

    wrote jacobi.glsl

--- src/jacobi.glsl
+++ src/jacobi.glsl
@@ -1,4 +1,33 @@
 #version 150
 
-out vec4 result;
+out vec4 xNew; //result
 
+uniform float alpha; 
+uniform float rBeta;
+//for pressure, alpha = -(dx^2)
+//              rbeta = 1/4
+//              x = pressure
+//              b = divergence
+//for diffusion, alpha = dx^2 /v*dt 
+//              rbeta = 1/(4 + dx^2 /v*dt)
+//              x = velocity
+//              d = velocity
+uniform sampler2D x;// x vector, Ax = b
+uniform sampler2D b;// b vector, Ax = b
+
+
+void main(){
+    ivec2 fragCoord = ivec2(gl_fragCoord.xy);
+    //get left, right, bottom, top samples.
+    vec4 top = texelFetchOffset(x, fragCoord, 0, ivec2(0, 1));
+    vec4 bot = texelFetchOffset(x, fragCoord, 0, ivec2(0, -1));
+    vec4 right = texelFetchOffset(x, fragCoord, 0, ivec2(1, 0));
+    vec4 left = texelFetchOffset(x, fragCoord, 0, ivec2(-1, 0));
+    
+
+    //get center of b sample
+    vec4 bC = texelFetch(b, fragCoord, 0);
+
+    xNew = (top + bot + right + left + alpha * bC) * rBeta;
+
+}//end jacobi main
